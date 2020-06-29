218082da96096fd8344c8829a82f1d4705b20a3f
jorgeazorin <jorgeazorin@gmail.com>
2020-06-29 15:15:59 +0200

    Updates

---
+++ precache-manifest.7bad90cd00304a97520f61dd317ff6e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96249d3f7b91e970c4d64068c7470276",
+    "url": "/mce-promotion-inspector/index.html"
+  },
+  {
+    "revision": "6091983d07b583dbed96",
+    "url": "/mce-promotion-inspector/static/css/main.8b32e31b.chunk.css"
+  },
+  {
+    "revision": "1e14fad7b579a5b6f43b",
+    "url": "/mce-promotion-inspector/static/js/2.ae1ee8a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mce-promotion-inspector/static/js/2.ae1ee8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6091983d07b583dbed96",
+    "url": "/mce-promotion-inspector/static/js/main.e2d63feb.chunk.js"
+  },
+  {
+    "revision": "23beb4b655dc663c9ca4",
+    "url": "/mce-promotion-inspector/static/js/runtime-main.423c0610.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mce-promotion-inspector/static/media/logo.5d5d9eef.svg"
+  }
+]);
